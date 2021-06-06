214844cb1b3cecdbb678f4cb0bbc0a6a0a41a4ae
mohamedrazzk <mohamedrazzk@gmail.com>
2021-06-06 22:19:58 +0200

    update

--- headers/main.c
+++ headers/main.c
@@ -3,9 +3,6 @@
 
 int main() {
 
-
-
-
     float _lat_float ;float _long_float ;
     char *gps_data = "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,05,2.68,129.0,M,50.1,M,,*42";
     get_lat_long(gps_data,&_lat_float,&_long_float);
@@ -16,12 +13,12 @@ int main() {
   printf("%f\n",_long_float); //float
 */
 
+
+
+
     float distance ,total_distance ;
-
     calc_distance(40.689202777778,74.044219444444,38.889069444444,77.034502777778,&distance);
-
     total_distance +=distance;
-
     printf("%f\n",distance);
     printf("The total distance = %f M",total_distance);
     return 0;
